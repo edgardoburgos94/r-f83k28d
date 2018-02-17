a78b3fc07c545bca5918d212679369a5c14cf43c
Edgardo David <edgardo.burgos94@gmail.com>
2018-02-17 17:57:42 -0500

    Increment

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.addNumber = this.addNumber.bind(this);
+
+    this.state = {
+      number: 0
+    }
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.number}</span>
+        <button id="inc" onClick={this.addNumber}>Incrementa</button>
       </div>
     );
+  }
+
+  addNumber() {
+    this.setState({
+      number: this.state.number + 1
+    });
   }
 }
 
